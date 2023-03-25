300bce653915584b67e1182aced2a9def162afe2
Omar Faruk <omar.faruk@outlook.com>
2023-03-25 13:37:55 +0000

    Initial page reader view

--- src/components/reader/index.jsx
+++ src/components/reader/index.jsx
@@ -1,7 +1,7 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 // 3rd party libraries
-import { Button, FloatButton, Skeleton, Watermark } from 'antd';
+import { Button, FloatButton, Skeleton } from 'antd';
 import ReactMarkdown from 'react-markdown'
 
 // Local Imports
@@ -17,61 +17,81 @@ import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 // };
 
 // 1. Mode  vertical or single-page or flip-book
-const Reader = ({ contents, loading, mode, t, font, size, lineHeight, hasPreviousChapter = false, onPreviousChapter = () => {}, hasNextChapter = false, onNextChapter = () => {} }) => {
+const Reader = ({ contents, loading, mode, font, size, lineHeight, hasPreviousChapter = false, onPreviousChapter = () => {}, hasNextChapter = false, onNextChapter = () => {} }) => {
     const ref = useRef(null);
-    const [page, setPage] = useState(0)
-    const pageWidth = ref.current ? ref.current.offsetWidth : 0
+    const refContents = useRef(null);
+    const [progress, setProgress] = useState(0)
+    const pageWidth = ref.current ? ref.current.offsetWidth + 24 : 0
+    const contentWidth = refContents.current ? refContents.current.scrollWidth : 0;
+    const pageCount = Math.round(contentWidth > 0 ? contentWidth / pageWidth : 0);
 
-    if (loading) {
-        return (<div className={styles.reader}>
-            <div className={styles['reader__vertical']}>
-                <Skeleton />
-            </div>
-        </div >)
-    }
+    console.log(`contentWidth : ${contentWidth}`)
+    useEffect(() => {
+        setProgress(0)
+    }, [mode, contents])
+
+    // if (loading) {
+    //     return (<div className={styles.reader}>
+    //         <div className={styles['reader__vertical']}>
+    //             <Skeleton />
+    //         </div>
+    //     </div >)
+    // }
 
     const onPrevious = () => { 
+        if (progress <= 0 && hasPreviousChapter) 
+        {
+            onPreviousChapter()
+            return true;
+        }
+
         if (!canGoPrevious()) return false;
         
-        setPage(page - 1)
+        setProgress(progress - 1)
         return true;
     }
     const canGoPrevious = () => {
         if (mode === 'vertical') return false;
-        return page > 0 || hasPreviousChapter;
+        return progress > 0 || hasPreviousChapter;
 
     }
     const onNext = () => {
+        if (progress >= pageCount - 1 && hasNextChapter) 
+        {
+            onNextChapter()
+            return true;
+        }
+
         if (!canGoNext()) return false;
 
-        setPage(page + 1)
+        setProgress(progress + 1)
         return true;
     }
     const canGoNext = () => {
         if (mode === 'vertical') return false;
-        return hasNextChapter;
+        return progress < pageCount - 1 || hasNextChapter; 
     }
 
-    const left = `${pageWidth * page}px`;
+    const left = `${pageWidth * progress}px`;
 
     const className = `${styles.reader} ${styles[mode]}`
 
-    const nextButton = canGoPrevious() ? (<Button className={`${styles['reader__nav']} ${styles['reader__nav--previous']}`} shape="circle" icon={<MdChevronLeft />} onClick={onPrevious} />) : null
-    const previousButton = canGoNext() ? (<Button className={`${styles['reader__nav']} ${styles['reader__nav--next']}`} shape="circle" icon={<MdChevronRight />} onClick={onNext} />) : null
+    const previousButton = canGoPrevious() ? (<Button className={`${styles['reader__nav']} ${styles['reader__page-number--previous']}`} data-ft="nextButton" shape="circle" icon={<MdChevronRight />} onClick={onPrevious} />) : null
+    const nextButton = canGoNext() ? (<Button className={`${styles['reader__nav']} ${styles['reader__page-number--next']}`} data-ft="previousButton" shape="circle" icon={<MdChevronLeft />} onClick={onNext} />) : null
 
 
     return (
     <div className={className} data-ft="reader-layout">
         {previousButton}
-        <div className={styles[`reader__container`]} ref={ref} data-ft="reader-container">
-            <div className={styles[`reader__contents`]} style={{ fontFamily: font, fontSize: size, lineHeight: lineHeight, left: left }}>
-                <Watermark content={t('app')} >
-                    <ReactMarkdown children={contents} />
-                </Watermark>
+        <div className={styles[`reader__container`]} ref={ref} data-ft="reader-container" >
+            <div className={styles[`reader__page`]} style={{ left: left }}>
+                    <div className={styles[`reader__contents`]} ref={refContents} style={{ fontFamily: font, fontSize: size, lineHeight: lineHeight }}>
+                        { loading ? <Skeleton /> : <ReactMarkdown children={contents} />}
+                </div>
             </div>
+                <div className={styles[`reader__page-number`]}>{progress + 1} / { pageCount }</div>
         </div>
         {nextButton}
-        <div className={styles[`reader__page-number`]}>{page + 1}</div>
         <FloatButton.BackTop />
     </div>)
         
